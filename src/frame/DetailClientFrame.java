6a91fa530f568bdf57afaa15d91b8439b7bcc380
WilliamG57 <gillet.william44@gmail.com>
2023-11-22 16:53:03 +0100

    Modif DetailClientFrame

--- src/frame/DetailClientFrame.java
+++ src/frame/DetailClientFrame.java
@@ -1,8 +1,10 @@
 package frame;
 
 import classmetier.*;
+import dao.MedecinDAO;
+import dao.MutuelleDAO;
+import dao.SpecialisteDAO;
 import service.ClientService;
-import utils.MyException;
 
 import javax.swing.*;
 import javax.swing.plaf.nimbus.NimbusLookAndFeel;
@@ -10,7 +12,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-import java.sql.SQLException;
 
 
 public class DetailClientFrame extends JFrame {
@@ -35,18 +36,22 @@ public class DetailClientFrame extends JFrame {
     private JTextField textTelephone;
     private JTextField textAdresse;
     private JTextField textSecu;
-    private JTextField textMutuelle;
-    private JTextField textMedecin;
     private JLabel medecinClient;
     private JButton btnRetour;
-    private JTextField textSpecialiste;
     private JLabel specialisteClient;
     private JButton btnModifier;
     private JTextField textId;
+    private JComboBox comboMedecin;
+    private JComboBox comboSpecialiste;
+    private JComboBox comboMutuelle;
     private boolean isNomEditable = false;
-    ClientService clientService = new ClientService();
+    private ClientService clientService = new ClientService();
+    private MedecinDAO medecinDAO = new MedecinDAO();
+    private SpecialisteDAO specialisteDAO = new SpecialisteDAO();
+    private MutuelleDAO mutuelleDAO = new MutuelleDAO();
 
-    public Clients DetailClientFrame() throws Exception {
+
+    public DetailClientFrame() throws Exception {
 
         try {
             UIManager.setLookAndFeel(new NimbusLookAndFeel());
@@ -60,6 +65,19 @@ public class DetailClientFrame extends JFrame {
         setContentPane(detailClientPanel);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         //TODO
+
+        for (Medecins medecins : medecinDAO.findAll()) {
+            comboMedecin.addItem(medecins);
+            comboMedecin.setSelectedIndex(-1);
+        }
+        for (Specialistes specialistes : specialisteDAO.findAll()) {
+            comboSpecialiste.addItem(specialistes);
+            comboSpecialiste.setSelectedIndex(-1);
+        }
+        for (Mutuelle mutuelle : mutuelleDAO.findAll()) {
+            comboMutuelle.addItem(mutuelle);
+            comboMutuelle.setSelectedIndex(-1);
+        }
         for (Clients clients : clientService.findAll()) {
             listeClient.addItem(clients.getNom());
             listeClient.setSelectedIndex(-1);
@@ -81,15 +99,14 @@ public class DetailClientFrame extends JFrame {
                         textPostal.setText(clients.getCodePostal());
                         textVille.setText(clients.getVille());
                         textSecu.setText(clients.getSecuriteSociale());
-                        textMutuelle.setText(clients.getMutuelle().getNom());
-                        textMedecin.setText(clients.getMedecin().getNom());
-                        textSpecialiste.setText(clients.getSpecialiste().getNom());
-                        System.out.println(clients.getNom());
+                        comboMutuelle.setSelectedItem(clients.getMutuelle());
+                        comboMedecin.setSelectedItem(clients.getMedecin());
+                        comboSpecialiste.setSelectedItem(clients.getSpecialiste());
                     }
                 }
             });
         }
-        textSpecialiste.addMouseListener(new MouseListener() {
+        comboSpecialiste.addMouseListener(new MouseListener() {
             @Override
             public void mouseClicked(MouseEvent e) {
                 GeneriqueFrame x = null;
@@ -122,94 +139,94 @@ public class DetailClientFrame extends JFrame {
             }
         });
 
-        textMedecin.addMouseListener(new MouseListener() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                try {
-                    GeneriqueFrame x = new GeneriqueFrame(textMedecin.getText(), "Medecin");
-                } catch (Exception ex) {
-                    throw new RuntimeException(ex);
-                }
-                setVisible(true);
-            }
+//        textMedecin.addMouseListener(new MouseListener() {
+//            @Override
+//            public void mouseClicked(MouseEvent e) {
+//                try {
+//                    GeneriqueFrame x = new GeneriqueFrame(textMedecin.getText(), "Medecin");
+//                } catch (Exception ex) {
+//                    throw new RuntimeException(ex);
+//                }
+//                setVisible(true);
+//            }
+//
+//            @Override
+//            public void mousePressed(MouseEvent e) {
+//
+//            }
+//
+//            @Override
+//            public void mouseReleased(MouseEvent e) {
+//
+//            }
+//
+//            @Override
+//            public void mouseEntered(MouseEvent e) {
+//
+//            }
+//
+//            @Override
+//            public void mouseExited(MouseEvent e) {
+//
+//            }
+//        });
 
-            @Override
-            public void mousePressed(MouseEvent e) {
-
-            }
-
-            @Override
-            public void mouseReleased(MouseEvent e) {
-
-            }
-
-            @Override
-            public void mouseEntered(MouseEvent e) {
-
-            }
-
-            @Override
-            public void mouseExited(MouseEvent e) {
-
-            }
-        });
-
-        btnModifier.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                textNom.setEditable(true);
-                textPrenom.setEditable(true);
-                textNaissance.setEditable(true);
-                textTelephone.setEditable(true);
-                textMail.setEditable(true);
-                textSpecialiste.setEditable(true);
-                textAdresse.setEditable(true);
-                textPostal.setEditable(true);
-                textVille.setEditable(true);
-                textSecu.setEditable(true);
-                textMutuelle.setEditable(true);
-                textMedecin.setEditable(true);
-                btnModifier.setText("Valider");
-                Object selectItem = comboMutuelle.getSelectedItem();
-                int muId = ((Mutuelle) selectItem).getMutId();
-                Object selectItem1 = comboMedecin.getSelectedItem();
-                int mId = ((Medecins) selectItem1).getMedId();
-                Object selectItem2 = comboSpecialiste.getSelectedItem();
-                int sId = ((Specialistes) selectItem2).getSpeId();
-                if (isNomEditable == true) {
-                    try {
-                        Clients cl = getClients(muId, mId, sId);
-                        clientService.modifierClient(new Clients(cl));
-                        JOptionPane.showMessageDialog(null, "Validé");
-                        isNomEditable = false;
-                    } catch (MyException ex) {
-                        throw new RuntimeException(ex);
-                    } catch (SQLException ex) {
-                        throw new RuntimeException(ex);
-                    }
-                }
-                isNomEditable = true;
-            }
-        });
-        private Clients getClients(int muId, int mId, int sId) {
-
-            String nom = textNom.getText();
-            String prenom = textPrenom.getText();
-            String adresse = textAdresse.getText();
-            String codePostal = textPostal.getText();
-            String ville = textVille.getText();
-            String telephone = textTelephone.getText();
-            String email = textMail.getText();
-            String securiteSocial = textSecu.getText();
-            String dateNaissance = textNaissance.getText();
-            int mutuelle = muId;
-            int medecin = mId;
-            int specialiste = sId;
-            Clients cl =
-                    new Clients(nom, prenom, adresse, codePostal, ville, telephone, email,
-                            securiteSocial, dateNaissance, mutuelle, medecin, specialiste);
-            return cl;
-        }
+//        btnModifier.addActionListener(new ActionListener() {
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+//                textNom.setEditable(true);
+//                textPrenom.setEditable(true);
+//                textNaissance.setEditable(true);
+//                textTelephone.setEditable(true);
+//                textMail.setEditable(true);
+//                textSpecialiste.setEditable(true);
+//                textAdresse.setEditable(true);
+//                textPostal.setEditable(true);
+//                textVille.setEditable(true);
+//                textSecu.setEditable(true);
+//                textMutuelle.setEditable(true);
+//                textMedecin.setEditable(true);
+//                btnModifier.setText("Valider");
+//                Object selectItem = comboMutuelle.getSelectedItem();
+//                int muId = ((Mutuelle) selectItem).getMutId();
+//                Object selectItem1 = comboMedecin.getSelectedItem();
+//                int mId = ((Medecins) selectItem1).getMedId();
+//                Object selectItem2 = comboSpecialiste.getSelectedItem();
+//                int sId = ((Specialistes) selectItem2).getSpeId();
+//                if (isNomEditable == true) {
+//                    try {
+//                        Clients cl = getClients(muId, mId, sId);
+//                        clientService.modifierClient(new Clients(cl));
+//                        JOptionPane.showMessageDialog(null, "Validé");
+//                        isNomEditable = false;
+//                    } catch (MyException ex) {
+//                        throw new RuntimeException(ex);
+//                    } catch (SQLException ex) {
+//                        throw new RuntimeException(ex);
+//                    }
+//                }
+//                isNomEditable = true;
+//            }
+//        });
+//        private Clients getClients(int muId, int mId, int sId) {
+//
+//            String nom = textNom.getText();
+//            String prenom = textPrenom.getText();
+//            String adresse = textAdresse.getText();
+//            String codePostal = textPostal.getText();
+//            String ville = textVille.getText();
+//            String telephone = textTelephone.getText();
+//            String email = textMail.getText();
+//            String securiteSocial = textSecu.getText();
+//            String dateNaissance = textNaissance.getText();
+//            int mutuelle = muId;
+//            int medecin = mId;
+//            int specialiste = sId;
+//            Clients cl =
+//                    new Clients(nom, prenom, adresse, codePostal, ville, telephone, email,
+//                            securiteSocial, dateNaissance, mutuelle, medecin, specialiste);
+//            return cl;
+        //  }
         btnRetour.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
